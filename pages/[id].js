c79a9f7027445416002a87cebaa951ab0468fb0d
Umut Toker <utoker@gmail.com>
2022-07-04 08:31:26 -0400

    remove comments

--- pages/[id].js
+++ pages/[id].js
@@ -1,39 +1,10 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-prototype-builtins */
+/* eslint-disable no-undef */
 import axios from '../component/Axios';
-// import { useRouter } from 'next/router';
-// import { useEffect,useState } from 'react';
+
 import FoodDetail from '../component/FoodDetail';
 
 const Details = ({ data }) => {
-  // const router = useRouter();
-  // const { id } = router.query;
-  // const apiKey = process.env.NEXT_PUBLIC_KEY;
-  // const apiKey = 'icBngbwgTFe5YAmzOf4teKUNCLfUnbO3m8vFwM09';
-  // const url = `/food/${id}?api_key=${apiKey}`;
-  // const [data, setData] = useState(null);
-  // const [isLoading, setIsLoading] = useState('');
-  // const [hasError, setHasError] = useState('');
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setIsLoading(true);
-  //     setHasError(false);
-
-  //     try {
-  //       const res = await axios.get(url);
-  //       setData(res.data);
-  //       setIsLoading(false);
-  //       return res;
-  //     } catch (error) {
-  //       setHasError(true);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [setData, url, setIsLoading, setHasError, axios]);
-
   let desc;
   if (data && data.foodAttributes.hasOwnProperty(2)) {
     desc = data.foodAttributes[2].value;
@@ -53,7 +24,6 @@ export default Details;
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
-  // const apiKey = 'icBngbwgTFe5YAmzOf4teKUNCLfUnbO3m8vFwM09';
   const APIKEY = process.env.APIKEY;
   const url = `/food/${id}?api_key=${APIKEY}`;
   const res = await axios.get(url);
